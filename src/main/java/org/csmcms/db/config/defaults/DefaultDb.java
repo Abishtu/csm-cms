9474adbcf97d111d259ce8d446a5058477eca43c
Developer <developer@srv1147362.hstgr.cloud>
2025-12-10 03:07:44 +0000

    Added a defaults class

---
+++ src/main/java/org/csmcms/db/config/defaults/DefaultDb.java
@@ -0,0 +1,7 @@
+package org.csmcms.db.config.defaults;
+
+public class DefaultDb {
+    public static final String Name = "postgres";
+    public static final String Driver = "org.postgresql.Driver";
+    public static final String Dialect = "org.hibernate.dialect.PostgresSQLDialect";
+}
